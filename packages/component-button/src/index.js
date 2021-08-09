4b1eae444f82988b6d1cc66714eabb50ec75f80b
Mauricio Ramirez <mfernande@finishline.com>
2021-08-09 00:22:01 -0600

    test

--- packages/component-button/src/index.js
+++ packages/component-button/src/index.js
@@ -5,6 +5,7 @@ import StyledButton from './styles/styledButton'
 const Button = ({ type = 'button', children, onClick }) => (
   <StyledButton type={type} className="button" onClick={onClick}>
     {children}
+    test
   </StyledButton>
 )
 
